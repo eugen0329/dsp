8e0ac3b896f7cb3730517fd5eaf743dd59a03bb5
eugen0329 <eugen0329@gmail.com>
2015-11-14 21:30:29 +0300

    Added complex num operations comments

--- fft/lib/fft/fft.js
+++ fft/lib/fft/fft.js
@@ -22,12 +22,15 @@ function FFT() {
     var halfLen = Math.floor(len / 2);
     var left  = [],
         right = [];
-    // var wn = math.complex({re: cos(PI/halfLen), im: direction * sin(PI/halfLen)}),
+
     var wn = math.complex({r: 1, phi: (-2.0 * PI)/len }),
         w  = math.complex(1, 0);
     for (var j = 0; j < halfLen; j++) {
+      // left[j] = vec[j] + vec[j + halfLen];
       left[j] = math.add(vec[j], vec[j + halfLen]);
+      // right[j] = w * (vec[j], vec[j + halfLen]);
       right[j] = math.multiply(math.subtract(vec[j], vec[j + halfLen]), w);
+      // w *= wn;
       w = math.multiply(w, wn);
     }
 

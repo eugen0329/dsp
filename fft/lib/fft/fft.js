f841804b5760ab6197b5c70071e890b9b55bddda
eugen0329 <eugen0329@gmail.com>
2015-11-14 21:28:27 +0300

    Fixed results merging, extracted fft/fft.js

---
+++ fft/lib/fft/fft.js
@@ -0,0 +1,50 @@
+var fft = new FFT()
+
+function FFT() {
+  var cos = Math.cos,
+      sin = Math.sin,
+      PI  = Math.PI;
+
+  var DIRECT  = 1,
+      INVERSE = -1;
+
+
+  this.direct = function(vec) {
+    return transform(vec, DIRECT)
+  }
+
+  function transform(vec, direction) {
+    var len = vec.length;
+    if (len == 1) {
+      return vec;
+    }
+
+    var halfLen = Math.floor(len / 2);
+    var left  = [],
+        right = [];
+    // var wn = math.complex({re: cos(PI/halfLen), im: direction * sin(PI/halfLen)}),
+    var wn = math.complex({r: 1, phi: (-2.0 * PI)/len }),
+        w  = math.complex(1, 0);
+    for (var j = 0; j < halfLen; j++) {
+      left[j] = math.add(vec[j], vec[j + halfLen]);
+      right[j] = math.multiply(math.subtract(vec[j], vec[j + halfLen]), w);
+      w = math.multiply(w, wn);
+    }
+
+    return merge(transform(left, 1), transform(right, 1));
+  }
+
+  var merge = function(left, right) {
+    var merged = [];
+    for (var j = 0, len = left.length; j < len; j++) {
+
+      merged[j * 2] = left[j];
+      merged[j * 2 + 1] = right[j];
+    }
+    return merged;
+  }
+
+  return this;
+}
+
+

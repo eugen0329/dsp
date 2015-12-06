215bfc8bb83338971f5b179469d7cf606a951846
eugen0329 <eugen0329@gmail.com>
2015-12-06 21:31:02 +0300

    Fixed convolution

--- convolution-and-correlation/lib/convolution.js
+++ convolution-and-correlation/lib/convolution.js
@@ -1,28 +1,16 @@
 var convolution = new Convolution();
 
 function Convolution() {
-  this.fft = function(first, second, samplingStep) {
-      var pres = 3;
-      var roundedDataset  = function(e, i) { return {x: math.round(i * samplingStep, 2), y: math.complex.abs(e)}; }
-      var yRound = function(e) {  return math.round(e, pres); };
-      var counts = 16;
-
-      firstValues  = sampler.apply(first, counts, samplingStep);
-      secondValues = sampler.apply(second, counts, samplingStep);
-
-
-      firstValuesRounded = firstValues.map(yRound);
-      secondValuesRounded = secondValues.map(yRound);
-
-
-      firstFftResults = fft.forward(firstValues);
+  this.fft = function(firstPoints, secondPoints) {
+      var firstFFTPoints = fft.forward(firstPoints);
       var firstFftComplexity = fft.complexity;
-      firstFftResultsRounded = firstFftResults.map(yRound);
 
-      secondFftResults = fft.forward(secondValues);
+      var secondFFTPoints = fft.forward(secondPoints);
       var secondFftComplexity = fft.complexity;
-      secondFftResultsRounded = secondFftResults.map(yRound);
-      var multipliedImages = firstFftResultsRounded.map(function(e, i) { return math.multiply(e, secondFftResults[i]); });
+
+
+      var multipliedImages = firstFFTPoints.map(function(e, i) { return math.multiply(e, secondFFTPoints[i]); });
+
       var convolutionResult = fft.inverse(multipliedImages);
       return convolutionResult;
     }

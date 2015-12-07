bdcd3ba4a26e61844b700f948a1f7cf39a3406ad
eugen0329 <eugen0329@gmail.com>
2015-12-08 01:25:31 +0300

    Add images-processing folder

---
+++ images-processing/main.cpp
@@ -0,0 +1,17 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+
+using namespace cv;
+
+const char* window_name = "ASD";
+
+int main(int argc, char *argv[])
+{
+    cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
+    while (1) {
+    }
+    return 0;
+}

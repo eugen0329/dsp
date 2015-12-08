a46af1bd25aefad1277607f8e7020cec394a5fdf
eugen0329 <eugen0329@gmail.com>
2015-12-08 14:40:53 +0300

    Add gamma correction with slider

--- images-processing/main.cpp
+++ images-processing/main.cpp
@@ -6,6 +6,71 @@
 using namespace std;
 using namespace cv;
 
+const char *SRC_IMAGE_WINDOW = "Src image";
+const char *GAMMA_IMAGE_WINDOW = "Gamma";
+
+Mat gammaCorrection(Mat &img, double gamma);
+void showIntensityHistogram(Mat &img);
+
+const int gammaSliderMax = 100;
+const int gammaMax = 4;
+int gammaSlider = gammaSliderMax / gammaMax;
+Mat img;
+
+void onGamma(int, void *)
+{
+    float gamma = (float)gammaSlider * gammaMax / gammaSliderMax;
+    Mat gammaApplied = gammaCorrection(img, gamma);
+
+    char buff[100];
+    snprintf(buff, sizeof(buff), "Gamma value: %.2f", gamma);
+
+    int thickness = 2;
+    putText(gammaApplied, buff, Point(20, 20), CV_FONT_HERSHEY_SIMPLEX, 0.5,
+            cv::Scalar(255), thickness, 8, false);
+    imshow(GAMMA_IMAGE_WINDOW, gammaApplied);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return -1;
+    img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if (!img.data)
+        return -2;
+
+    Mat gammaApplied = gammaCorrection(img, 0.6);
+    /* gammaCorrection(img,gammaApplied, 0.2); */
+
+    namedWindow(SRC_IMAGE_WINDOW, WINDOW_AUTOSIZE);
+    imshow(SRC_IMAGE_WINDOW, img);
+
+    namedWindow(GAMMA_IMAGE_WINDOW, WINDOW_AUTOSIZE);
+    /* imshow(GAMMA_IMAGE_WINDOW, gammaApplied); */
+    createTrackbar("Adjust Gamma", GAMMA_IMAGE_WINDOW, &gammaSlider,
+                   gammaSliderMax, onGamma);
+    onGamma(0, 0);
+
+    /* showIntensityHistogram(img); */
+    waitKey(0);
+    return 0;
+}
+
+Mat gammaCorrection(Mat &img, double gamma)
+{
+    double inverseGamma = 1.0 / gamma;
+
+    Mat lutMatrix(1, 256, CV_8UC1);
+    uchar *ptr = lut_matrix.ptr();
+    for (int i = 0; i < 256; i++)
+        ptr[i] = (int)(pow((double)i / 255.0, inverseGamma) * 255.0);
+
+    Mat result;
+    LUT(img, lutMatrix, result);
+
+    return result;
+}
+
 void showIntensityHistogram(Mat &img)
 {
     int histSize = 256; // bin size
@@ -48,13 +113,3 @@ void showIntensityHistogram(Mat &img)
     imshow("Result", histImage);
 }
 
-int main(int, char **)
-{
-    Mat img = imread("1.jpg", CV_LOAD_IMAGE_COLOR);
-    namedWindow("img", WINDOW_AUTOSIZE);
-    imshow("img", img);
-
-    showIntensityHistogram(img);
-    waitKey(0);
-    return 0;
-}

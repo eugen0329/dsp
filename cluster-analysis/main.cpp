6980f58fd50a5b82900e2f222c2ce2b5413ddfaa
eugen0329 <eugen0329@gmail.com>
2016-04-04 22:37:32 +0300

    Implement image binarization

--- cluster-analysis/main.cpp
+++ cluster-analysis/main.cpp
@@ -11,20 +11,25 @@
 using namespace std;
 using namespace cv;
 
+void filter(Mat &from, Mat &to);
+
 int main(int argc, char* argv[])
 {
 
-    Mat img;
+    Mat img_mat;
     if (argc < 2)
         return -1;
-    img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    if (!img.data)
+    img_mat = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if (img_mat.empty())
         return -2;
 
+    Mat grayscale_mat (img_mat.size(), CV_8U);
+    cvtColor(img_mat, grayscale_mat, CV_BGR2GRAY );
 
-    imshow("image", img);
+    Mat binary_mat(grayscale_mat.size(), grayscale_mat.type());
+    threshold(grayscale_mat, binary_mat, 200, 255, cv::THRESH_BINARY);
 
-    waitKey(0);
+    imshow("image", binary_mat);
 
     waitKey(0);
     return 0;

cd8b088cf236916c080c83489f8d3e96975d7615
eugen0329 <eugen0329@gmail.com>
2016-04-04 21:48:56 +0300

    Initial cluster-anilizis project setup

---
+++ cluster-analysis/main.cpp
@@ -0,0 +1,18 @@
+// Алгоритм выделения: связных областейПоследовательного сканирования
+// Алгоритм кластеризации: k-means
+
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
